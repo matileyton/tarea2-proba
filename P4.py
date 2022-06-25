7eb26a7c92aebfc69c63336dcd55ee338cd6c6c2
almandozm <matiasalmandozx@gmail.com>
2022-06-24 20:51:56 -0400

    Lista

--- P4.py
+++ P4.py
@@ -4,7 +4,32 @@ import numpy as np
 import scipy.stats as st
 from matplotlib import pyplot as plt
 
+#INTERVALOS DE CONFIANZA
 monedas = ['Bitcoin', 'Dogecoin', 'Ethereum']
+for m in range(len(monedas)):
+    csv = 'coin_' + monedas[m] + '.csv'
+    
+    df = pd.read_csv(csv) 
+    df["Prom"] = (df["Open"] + df["Close"])/2 - df["Open"]
+
+    df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+    df['Year'] = df['Date'].dt.year
+
+    df2017 = df[df['Year'] == 2017]
+    promedios_bc_2017 = df2017['Prom'].tolist()
+
+    df2018 = df[df['Year'] == 2018]
+    promedios_bc_2018 = df2018['Prom'].tolist()
+    
+    print('')
+    print(f'Intervalo de confianza {monedas[m]} (2017-2018)')
+    interval_17 = st.norm.interval(0.95, loc=np.mean(promedios_bc_2017), scale=st.sem(promedios_bc_2017))
+    print(f'2017: {interval_17}')
+
+    interval_18 = st.norm.interval(0.95, loc=np.mean(promedios_bc_2018), scale=st.sem(promedios_bc_2018))
+    print(f'2018: {interval_18}')
+
+#GRAFICOS PROMEDIOS MENSUALES
 for m in range(len(monedas)):
     csv = 'coin_' + monedas[m] + '.csv'
     
@@ -18,16 +43,8 @@ for m in range(len(monedas)):
     promedios_bc_2017 = df2017['Prom'].tolist()
 
     df2018 = df[df['Year'] == 2018]
-    promedios_bc_2018 = df2018['Prom'].tolist()
+    promedios_bc_2018 = df2018['Prom'].tolist()    
 
-    '''
-    interval_17 = st.norm.interval(0.99, loc=np.mean(promedios_bc_2017), scale=st.sem(promedios_bc_2017))
-    print(interval_17[1] - interval_17[0])
-
-    interval_18 = st.norm.interval(0.99, loc=np.mean(promedios_bc_2018), scale=st.sem(promedios_bc_2018))
-    print(interval_18[1] - interval_18[0])
-    '''
-    
     meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septimebre', 'Octubre', 'Noviembre', 'Diciembre']
     dias_meses = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
